65a4b40cc5ddc785bb8bfbed7695db84baaea2cf
Steven Johnson <sjohnson21960@gmail.com>
2020-08-21 10:34:02 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
+// Requiring necessary npm packages
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 3001;
+const db = require("./models");
+
+// Creating express app and configuring middleware needed for authentication
 const app = express();
-const PORT = process.env.PORT || 3007;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/api/test", function (req, res) {
-    res.json({
-        test: "test"
-    })
-})
+// Requiring our routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-app.get("*", function (req, res) {
-    res.sendFile("./client/build/idenx.html");
-})
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});

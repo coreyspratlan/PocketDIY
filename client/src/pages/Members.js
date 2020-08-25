1815492383cd4aebc32d3e2475033fdfc0d79f64
Steven Johnson <sjohnson21960@gmail.com>
2020-08-25 10:12:03 -0500

    API project post update

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import { useStoreContext } from '../utils/GlobalStore';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Projects from "./Projects.js";
 import Nav from "../components/Nav";
+import API from "../../src/utils/PROJECT_API"
 
 function Members() {
+
+  const newProject = (event) => {
+    const data = {
+      shape: "circle",
+      width: 5,
+      height: 5,
+      depth: 5,
+      radius: 5,
+      area: 5,
+      parimeter: 5
+    };
+    API.createProject(data)
+    .catch(err => {
+        alert("An error has occured")
+    })
+}
+
   const [state] = useStoreContext();
   const { email } = state;
   return (
@@ -17,6 +35,7 @@ function Members() {
               <div className="row">
                 <div className="col-md-6 col-md-offset-3">
                   <h2>Welcome {email}</h2>
+                  <button onClick={() => newProject()}>Test Project Save</button>
                 </div>
               </div>
             </div>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,5 +13,14 @@ export default {
     },
     logout: () => {
         return axios.get("/logout");
+    },
+    createProject: (data) => {
+        return axios.post("/api/projects", data);
+    },
+    getProjects: () => {
+        return axios.get("/api/projects");
+    },
+    deleteProjects: () => {
+        return axios.get("/api/projects/:id");
     }
 }

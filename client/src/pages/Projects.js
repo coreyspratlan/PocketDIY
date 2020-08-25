9486e836970f932303265b01a3bace17459dd34c
Steven Johnson <sjohnson21960@gmail.com>
2020-08-25 13:41:03 -0500

    get API updated

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,19 +1,66 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import SavedProject from "../components/ProjectSection/savedProject";
+import { SavedProject } from "../components/ProjectSection/savedProject";
+import API from "../utils/PROJECT_API"
+import { ListItem } from "../components/List";
+// class Projects extends Component 
+function Projects() {
 
-class Projects extends Component {
-  render() {
-    return (
-      <Container fluid>
-        <h1>Projects</h1>
+  const [projects, setProjects] = useState([])
 
-        <SavedProject />
-      </Container>
-    );
-  }
+  useEffect(() => {
+    loadProjects()
+  }, [])
+
+  function loadProjects() {
+    API.getProjects()
+      .then(res =>
+        setProjects(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <Container fluid>
+      <h1>Projects</h1>
+
+      {projects.map(project => {
+        return (
+          <ListItem key={project._id}>
+            <a href={"/projects/" + project._id}>
+            </a>
+            <h2>Created by: {project.UserId}
+            </h2>
+            <p>
+              Shape: {project.shape}
+            </p>
+            <p>
+              Width: {project.width}
+            </p>
+            <p>
+              Height: {project.height}
+            </p>
+            <p>
+              Depth: {project.depth}
+            </p>
+            <p>
+              Radius: {project.radius}
+            </p>
+            <p>
+              Area: {project.area}
+            </p>
+            <p>
+              Perimeter: {project.perimeter}
+            </p>
+          </ListItem>
+        );
+      })}
+
+    </Container>
+  );
+
 }
 
 

69431b3a7f872b2c7cfc311ba70929bdcd4f67ef
Steven Johnson <sjohnson21960@gmail.com>
2020-08-26 11:52:13 -0500

    else if shape submission states

--- client/src/components/InputCard/squareCard.js
+++ client/src/components/InputCard/squareCard.js
@@ -1,13 +1,115 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import '../InputCard/card.css';
 import ProjectArea from "../ProjectSection/projectArea";
-import Footer from "../Footer/Footer";
 
-function SquareCard() {
+class SquareCard extends Component{
+
+    handleFormSubmit = event => {
+        const { Name, Width, Height, Perimeter, Area, Unit } = this.state
+        event.preventDefault();
+        if (Width.trim().length === 0 && Height.trim().length === 0) {
+            alert('Fill out Width or Height');
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: ""
+            });
+            return;
+        } else if (Width.trim().length !== 0 && Height.trim().length !== 0) {
+            alert('Fill out Width  OR  Height');
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: ""
+            });
+            return;
+        } else if (Name.trim().length !== 0) {
+            alert('Fill out a project name');
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: ""
+            });
+            return;
+        }else if (Unit === 0) {
+            alert('Please select a unit');
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: ""
+            });
+            return;
+        } else if (Unit === 1 && Width.trim().length === 0) {
+            Height = Height.trim() * 2.54;
+            Perimeter = Height * 4;
+            Area = Height * Height
+            let newSquare = { Name, Width, Height, Perimeter, Area, Unit }
+            this.setState({ Square: newSquare })
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: "",
+                Unit: ""
+            });
+        } else if (Unit === 1 && Height.trim().length === 0) {
+            Width = Width.trim() * 2.54;
+            Perimeter = Width * 4;
+            Area = Width * Width
+            let newSquare = { Name, Width, Height, Perimeter, Area, Unit }
+            this.setState({ Square: newSquare })
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: "",
+                Unit: ""
+            });
+        } 
+        else if (Height.trim().length === 0) {
+            Perimeter = Width * 4;
+            Area = Width * Width
+            let newSquare = { Name, Width, Height, Perimeter, Area, Unit }
+            this.setState({ Square: newSquare })
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: "",
+                Unit: ""
+            });
+        } else {
+            Perimeter = Height * 4;
+            Area = Height * Height
+            let newSquare = { Name, Width, Height, Perimeter, Area, Unit }
+            this.setState({ Square: newSquare })
+            this.setState({
+                Name: "",
+                Width: "",
+                Height: "",
+                Perimeter: "",
+                Area: "",
+                Unit: ""
+            });
+        };
+    };
+
+    render () {
     return (
         <div className='inputAreas'>
             <h5> Square </h5>
@@ -49,7 +151,7 @@ function SquareCard() {
                 </Row>
 
                 <Col xs="auto" className="my-1">
-                    <Button type="submit">Submit</Button>
+                    <Button type="submit" onClick={this.handleFormSubmit} >Submit</Button>
                 </Col>
             </Form>
             <div className="projectSection">
@@ -57,6 +159,7 @@ function SquareCard() {
             </div>
         </div>
     )
+    }
 }
 
 export default SquareCard;

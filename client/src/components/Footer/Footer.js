6ed746c657b2f542635156deb1f5be4ecd9d661a
waltrevino <27642702+Waltrevino@users.noreply.github.com>
2020-08-28 14:49:58 -0500

    add project card and modify footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 
-
 function Footer() {
 
     const styles= {
         borderColor: "black",
-        backgroundColor: "lightgray"
+        backgroundColor: "lightgray",
+        height: 50
     }
 
-
     return (
-        <div style={styles}>
-            <Card>
+
+        <div style={styles} className="footer">
+      
+            This is a Footer
+      
+            {/* <Card>
                 <Card.Footer>This is a Footer</Card.Footer>
-            </Card>
+            </Card> */}
         </div>
     )
 }
